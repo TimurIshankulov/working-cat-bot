d8b26f814c4648d7c932f7f456417547860689b4
Timur Ishankulov <tim290592@gmail.com>
2022-07-05 16:23:25 +0300

    Add new fields for User model

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from audioop import add
 import shelve
 from config import shelve_users
 
@@ -13,16 +14,22 @@ class User(object):
         
         self.current_work = None
         self._level = 1
-        self._experience = 0
+        self._experience = 0.0
         self._until_level = 10
         self.coins = 0
 
+        self.toy_mouse_acquired = False
+        self.toy_bow_acquired = False
+        self.toy_ball_acquired = False
+        self._experience_multiplier = 1
+
         self.work_timer_dict = {'wash_dish': 15, 'vacuum': 15, 'bake': 15, 'tiktok': 15,
                                 'advertisement': 15}
-        self.work_experience_dict = {'wash_dish': 5, 'vacuum': 7, 'bake': 10, 'tiktok': 14,
+        self.work_experience_dict = {'wash_dish': 50, 'vacuum': 7, 'bake': 10, 'tiktok': 14,
                                      'advertisement': 19}
-        self.work_coins_dict = {'wash_dish': 5, 'vacuum': 7, 'bake': 10, 'tiktok': 14,
-                                'advertisement': 19}
+        self.work_coins_dict = {'wash_dish': 200, 'vacuum': 5, 'bake': 7, 'tiktok': 10,
+                                'advertisement': 15}
+        self.toy_cost_dict = {'toy_mouse': 100, 'toy_bow': 200, 'toy_ball': 300}
         
 
     def __str__(self):
@@ -63,12 +70,28 @@ class User(object):
     def until_level(self, until_level):
         self._until_level = until_level
 
+    @property
+    def experience_multiplier(self):
+        additional_coef = 0
+        if self.toy_mouse_acquired:
+            additional_coef += 0.1
+        if self.toy_bow_acquired:
+            additional_coef += 0.2
+        if self.toy_ball_acquired:
+            additional_coef += 0.3
+        return self._experience_multiplier + additional_coef
+
+    @experience_multiplier.setter
+    def experience_multiplier(self, experience_multiplier):
+        self._experience_multiplier = experience_multiplier
+
     def save(self):
         """Saves user class instance"""
         with shelve.open(shelve_users) as users:
             users[self.id] = self
 
     def level_up(self):
-        self.experience = self.experience - self.level * 10
-        self.level += 1
-        self.until_level = self.level * 10
+        while self._experience >= self.until_level:
+            self._experience = self._experience - self.level * 10
+            self.level += 1
+            self.until_level = self.level * 10
