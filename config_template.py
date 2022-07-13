693419547533b53e643021a055d24859f919ca5c
Timur Ishankulov <tim290592@gmail.com>
2022-07-13 17:33:41 +0300

    Add template for config

---
+++ config_template.py
@@ -0,0 +1,7 @@
+telegram_token = 'Your telegram bot token'
+
+log_file = 'bot.log'
+
+sqlite_users = 'data/users.sqlite'
+sqlite_timers = 'data/timers.sqlite'
+sqlite_cat_committee = 'data/cat_committee.sqlite'
