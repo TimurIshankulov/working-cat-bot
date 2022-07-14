6265afdacd41d6f53f43e723ace3af45bf0ae0d7
Timur Ishankulov <tim290592@gmail.com>
2022-07-14 13:12:59 +0300

    Change coins rewards for work and treasure hunt

--- info.py
+++ info.py
@@ -7,8 +7,8 @@ work_timer_dict = {'wash_dish': 10*60, 'vacuum': 20*60, 'bake': 30*60, 'tiktok':
                    'advertisement': 50*60}
 work_experience_dict = {'wash_dish': 5, 'vacuum': 7, 'bake': 10, 'tiktok': 14,
                         'advertisement': 19}
-work_coins_dict = {'wash_dish': 2, 'vacuum': 5, 'bake': 7, 'tiktok': 10,
-                   'advertisement': 15}
+work_coins_dict = {'wash_dish': 5, 'vacuum': 10, 'bake': 17, 'tiktok': 25,
+                   'advertisement': 40}
 toy_cost_dict = {'toy_mouse': 100, 'toy_bow': 200, 'toy_ball': 300}
 food_cost_dict = {'food_fish': 100, 'food_premium': 200, 'food_shrimp': 300}
 home_coin_cost_dict = {'home_small': 300, 'home_flat': 600, 'home_house': 1000, 'home_mansion': 3000}
@@ -16,7 +16,7 @@ home_gem_cost_dict = {'home_small': 10, 'home_flat': 25, 'home_house': 50, 'home
 
 treasure_hunt_timer_dict = {'solo': 60*60, 'treasure': 90*60, 'expredition': 120*60}
 #treasure_hunt_timer_dict = {'solo': 15, 'treasure': 15, 'expedition': 15}
-treasure_hunt_coins_dict = {'solo': 10, 'treasure': 20, 'expedition': 30}
+treasure_hunt_coins_dict = {'solo': 30, 'treasure': 50, 'expedition': 70}
 treasure_hunt_gems_dict = {'solo': 2, 'treasure': 4, 'expedition': 6}
 
 donate_cost_dict = {'donate_tea': 50, 'donate_paper': 100, 'donate_renovation': 200}
