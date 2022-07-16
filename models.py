6dc71bab0b3aa31828910fe7b30085f6c8bdb23d
Timur Ishankulov <tim290592@gmail.com>
2022-07-16 14:08:44 +0300

    Add MySQL tables with SQLAlchemy

--- models.py
+++ models.py
@@ -1,7 +1,18 @@
-from sqlitedict import SqliteDict
-from config import sqlite_users, sqlite_cat_committee
+import pickle
 
-class Base():
+from sqlalchemy import Column, Integer, String, BLOB, Float, Boolean
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import conn_string
+
+DeclarativeBase = declarative_base()
+engine_working_cat = create_engine(conn_string)
+DBSession_working_cat = sessionmaker(bind=engine_working_cat)
+db_session = DBSession_working_cat()
+
+class Base(DeclarativeBase):
     def __init__(self) -> None:
         self._level = 1
         self._experience = 0.0
@@ -33,9 +44,22 @@ class Base():
             self.level += 1
             self.until_level = self.level * self.experience_coef_level
 
+    #====== Table options ======#
+
+    __tablename__ = 'users'
+
+    id = Column(String(20), primary_key=True, nullable=False)
+
+    _level = Column(Integer())
+    _experience = Column(Float())
+    until_level = Column(Integer())
+    experience_coef_level = Column(Integer)
+    coins = Column(Float())
+
 
 class User(Base):
-    def __init__(self, id='', username='', fullname='', chat_id=0, cat_name='', status='new'):
+    def __init__(self, id='', username='', fullname='', chat_id=0, cat_name='Неизвестный кот',
+                 status='new'):
         Base.__init__(self)
         self.experience_coef_level = 10
 
@@ -75,6 +99,46 @@ class User(Base):
         self.trophies = {'a': False, 'b': False, 'c': False, 'd': False, 'e': False,
                          'f': False, 'g': False, 'h': False, 'i': False, 'j': False}
 
+    #====== Table options ======#
+
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}    
+
+    username = Column(String(50))
+    fullname = Column(String(60))
+    chat_id = Column(Integer())
+    cat_name = Column(String(50))
+    status = Column(String(50))
+
+    current_work = Column(String(50))
+    is_working = Column(Boolean())
+    current_treasure_hunt = Column(String(50))
+    is_treasure_hunting = Column(Boolean())
+
+    gems = Column(Integer())
+
+    toy_mouse_acquired = Column(Boolean())
+    toy_bow_acquired = Column(Boolean())
+    toy_ball_acquired = Column(Boolean())
+    _experience_multiplier = Column(Float())
+
+    food_fish_acquired = Column(Boolean())
+    food_premium_acquired = Column(Boolean())
+    food_shrimp_acquired = Column(Boolean())
+    _speed_multiplier = Column(Float())
+
+    home_small_acquired = Column(Boolean())
+    home_flat_acquired = Column(Boolean())
+    home_house_acquired = Column(Boolean())
+    home_mansion_acquired = Column(Boolean())
+    _coins_multiplier = Column(Float())
+
+    experience_donated = Column(Float())
+    coins_donated = Column(Integer())
+    trophies = Column(BLOB())
+
+    #====== Methods ======#
+
     def __str__(self):
         return ('ID: {self.id}, \
                  Username: {self.username}, \
@@ -82,10 +146,7 @@ class User(Base):
                  Chat ID: {self.chat_id}, \
                  Status: {self.status}'.format(self=self))
 
-    @property
-    def log_str(self):
-        """Returns log string"""
-        return '{self.fullname} <{self.username}>'.format(self=self)
+    #====== Properties ======#
 
     @property
     def experience_multiplier(self):
@@ -132,22 +193,165 @@ class User(Base):
     def coins_multiplier(self, coins_multiplier):
         self._coins_multiplier = coins_multiplier
 
+    #====== Methods ======#
+
     def save(self):
-        """Saves user class instance"""
-        db = SqliteDict(sqlite_users)
-        db[self.id] = self
-        db.commit()
-        db.close()
+        user_was_none = False
+        db_session = DBSession_working_cat()
+        user = db_session.query(User).filter_by(id=self.id).first()
+        if user is None:
+            user = self
+            user_was_none = True
+        
+        user._level = self.level
+        user._experience = self._experience
+        user.until_level = self.until_level
+        user.experience_coef_level = self.experience_coef_level
+        user.coins = self.coins
+
+        user.username = self.username
+        user.fullname = self.fullname
+        user.chat_id = self.chat_id
+        user.cat_name = self.cat_name
+        user.status = self.status
+
+        user.current_work = self.current_work
+        user.is_working = self.is_working
+        user.current_treasure_hunt = self.current_treasure_hunt
+        user.is_treasure_hunting = self.is_treasure_hunting
+
+        user.gems = self.gems
+
+        user.toy_mouse_acquired = self.toy_mouse_acquired
+        user.toy_bow_acquired = self.toy_bow_acquired
+        user.toy_ball_acquired = self.toy_ball_acquired
+        user._experience_multiplier = self._experience_multiplier
+
+        user.food_fish_acquired = self.food_fish_acquired
+        user.food_premium_acquired = self.food_premium_acquired
+        user.food_shrimp_acquired = self.food_shrimp_acquired
+        user._speed_multiplier = self._speed_multiplier
+
+        user.home_small_acquired = self.home_small_acquired
+        user.home_flat_acquired = self.home_flat_acquired
+        user.home_house_acquired = self.home_house_acquired
+        user.home_mansion_acquired = self.home_mansion_acquired
+        user._coins_multiplier = self._coins_multiplier
+
+        user.experience_donated = self.experience_donated
+        user.coins_donated = self.coins_donated
+        user.trophies = pickle.dumps(self.trophies)
+
+        if user_was_none:
+            db_session.add(user)
+        db_session.commit()
+        db_session.close()
 
 
-class CatCommittee(Base):
+class CatCommittee(DeclarativeBase):
     def __init__(self) -> None:
-        Base.__init__(self)
+        self._level = 1
+        self._experience = 0.0
         self.until_level = 100
         self.experience_coef_level = 100
+        self.coins = 0
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        self._level = level
+
+    @property
+    def experience(self):
+        return self._experience
+
+    @experience.setter
+    def experience(self, experience):
+        self._experience = experience
+        if self._experience >= self.until_level:
+            self.level_up()
+
+    def level_up(self):
+        while self._experience >= self.until_level:
+            self._experience = self._experience - self.level * self.experience_coef_level
+            self.level += 1
+            self.until_level = self.level * self.experience_coef_level
+
+    #====== Table options ======#
+
+    __tablename__ = 'cat_committee'
+
+    id = Column(String(20), primary_key=True, nullable=False)
+
+    _level = Column(Integer())
+    _experience = Column(Float())
+    until_level = Column(Integer())
+    experience_coef_level = Column(Integer)
+    coins = Column(Float())
 
     def save(self):
-        db = SqliteDict(sqlite_cat_committee)
-        db['cat_committee'] = self
-        db.commit()
-        db.close()
+        cat_committee_was_none = False
+        db_session = DBSession_working_cat()
+
+        cat_committee = db_session.query(CatCommittee).filter_by(id='cat_committee').first()
+        if cat_committee is None:
+            cat_committee = self
+            cat_committee_was_none = True
+        cat_committee._level = self._level
+        cat_committee._experience = self._experience
+        cat_committee.until_level = self.until_level
+        cat_committee.experience_coef_level = self.experience_coef_level
+        cat_committee.coins = self.coins
+
+        if cat_committee_was_none:
+            cat_committee.id = 'cat_committee'
+            db_session.add(cat_committee)
+        db_session.commit()
+        db_session.close()
+
+class Timer(DeclarativeBase):
+    def __init__(self, id=None, user=None, chat_id=None, message_id=None, start_timestamp=None,
+                 seconds=None, timer_type=None):
+        self.id = id
+        self.user = user
+        self.chat_id = chat_id
+        self.message_id = message_id
+        self.start_timestamp = start_timestamp
+        self.seconds = seconds
+        self.timer_type = timer_type
+
+    #====== Table options ======#
+
+    __tablename__ = 'timers'
+
+    id = Column(Integer(), primary_key=True, nullable=False, autoincrement=True)
+    user = Column(BLOB())
+    chat_id = Column(Integer())
+    message_id = Column(Integer())
+    start_timestamp = Column(Integer())
+    seconds = Column(Integer())
+    timer_type = Column(String(50))
+
+    def save(self):
+        db_session = DBSession_working_cat()
+
+        timer = self
+        timer.user = pickle.dumps(self.user)
+        timer.chat_id = self.chat_id
+        timer.message_id = self.message_id
+        timer.start_timestamp = self.start_timestamp
+        timer.seconds = self.seconds
+        timer.timer_type = self.timer_type
+
+        db_session.add(timer)
+        db_session.commit()
+        db_session.refresh(timer)
+        self.id = timer.id
+        db_session.close()
+        
+
+DeclarativeBase.metadata.create_all(engine_working_cat)
+DeclarativeBase.metadata.bind = engine_working_cat
